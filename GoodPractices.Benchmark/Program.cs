a9224d8f37f4fabc17bef86485c1070822ac2dc2
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2018-11-12 15:25:47 +0100

    Allow for test selection from ConsoleMenu

--- GoodPractices.Benchmark/Program.cs
+++ GoodPractices.Benchmark/Program.cs
@@ -1,41 +1,76 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Sockets;
+using System.Reflection;
 using System.Text;
 using System.Threading;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Diagnosers;
+using BenchmarkDotNet.Loggers;
 using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+using ConsoleTools;
 using GoodPractices.Benchmark.Lib.Http;
 using GoodPractices.Benchmark.Test.Collections;
 
 namespace GoodPractices.Benchmark
 {
-  public class Config : ManualConfig
-  {
-    public Config()
-    {
-      Add(DefaultConfig.Instance);
-      Add(MemoryDiagnoser.Default);
-    }
-  }
-
   class Program
   {
     static void Main(string[] args)
     {
-      // var summary = BenchmarkRunner.Run<StringConcatTest>(new Config { });
-      // var summary = BenchmarkRunner.Run<StringBuilderAppendTest>(new Config { });
-      // var summary = BenchmarkRunner.Run<PooledStringBuilderAppendTest>(new Config { });
-      // var summary = BenchmarkRunner.Run<PooledStringBuilderCopyToTest>(new Config { });
-      // var summary = BenchmarkRunner.Run<ReadFileTest>(new Config { });
-      // var summary = BenchmarkRunner.Run<RegexVsStringMatchingTest>(new Config { });
-      // var summary1 = BenchmarkRunner.Run<HttpTests>(new Config { });
-      // var summary2 = BenchmarkRunner.Run<HttpPipelineTests>(new Config { });
-      // var summary3 = BenchmarkRunner.Run<HttpBufferedPipelineTests>(new Config { });
-      var summary4 = BenchmarkRunner.Run<ReturnYieldVsReturnList>(new Config { });
+      var config = ManualConfig.CreateEmpty()
+        .With(JitOptimizationsValidator.DontFailOnError)
+        .With(MemoryDiagnoser.Default)
+        .With(ConsoleLogger.Default);
+      
+      var menu = new ConsoleMenu();
+
+      var testTypes = CollectTypes();
+
+      menu.Add("All", () => RunBenchmark(testTypes, config));
+
+      foreach (var type in testTypes)
+      {
+        menu.Add(type.Name, () => RunBenchmark(type, config));          
+      }
+
+      menu.Configure(menuConfig => 
+        {
+          menuConfig.ItemForegroundColor = ConsoleColor.White;
+          menuConfig.SelectedItemForegroundColor = ConsoleColor.Green;
+          menuConfig.ClearConsole = true;
+        }
+      );
+
+      menu.Show();
+    }
+
+    private static void RunBenchmark(Type type, IConfig config)
+    {
+      BenchmarkRunner.Run(type, config);
+    }
+
+    private static void RunBenchmark(Type[] types, IConfig config)
+    {
+      foreach (var type in types)
+      {
+        BenchmarkRunner.Run(type, config);          
+      }
+    }
+
+    private static Type[] CollectTypes()
+    {
+      var classesWithAttribute = typeof(BenchmarkDotNet.Attributes.BenchmarkAttribute);
+
+      return Assembly.GetEntryAssembly().GetTypes()
+          .Where(t => t.GetMethods().Any(m => m.GetCustomAttributes(classesWithAttribute, false).Length > 0))
+          .OrderBy(c => c.Name)
+          .ToArray();
     }
   }
 }
